2f9d6df99f4e551bf3f393f4710b0ab55253c28a
javihzz <javiersotohernandez@hotmail.com>
2024-06-20 14:55:18 +0000

    Mundo guardado 2024-06-20

--- servidor.py
+++ servidor.py
@@ -54,4 +54,4 @@ if flnm.split(".")[-1] == "pyc":
     os.system(f"python3 {flnm}")
 else:
 	os.system(f"chmod +x {flnm} && ./{flnm}")          
-	                                
+	                            
